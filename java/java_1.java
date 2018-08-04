02945cb689661ffab1941bb96ffeb9fe7bbdb823
Kunal Yadav <39200197+kunaly1038@users.noreply.github.com>
2018-08-04 14:05:38 +0530

    Create java_1.java

---
+++ java/java_1.java
@@ -0,0 +1,24 @@
+/**Display the most famous phrase of beginning any programming language, "Hello World" (without Quotes).
+
+Input:
+You do not  need to take any input for this problem
+
+Output:
+Display "Hello World" (without quotes) on screen.
+
+Example:
+Output:
+Hello World**/
+
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		System.out.println("Hello World");
+	}
+}
+
